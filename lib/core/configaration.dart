1998d36fdfbf03e00cb1436b3a9a719eeba08c10
Ayabonga <ayabongabooi2@gmail.com>
2023-09-20 15:13:40 +0200

    chore(configuration): added list of banned countries with thier card type

---
+++ lib/core/configaration.dart
@@ -0,0 +1,22 @@
+class Configuration {
+  final List<Map<String, dynamic>> bannedListOfCountry = [
+    {
+      "russia": ["visa", "master"],
+    },
+    {
+      "iran": ["visa", "master"],
+    },
+    {
+      "Sudan": ["visa", "master"],
+    },
+    {
+      "sudan": ["visa", "master"],
+    },
+    {
+      "syria": ["visa", "master"],
+    },
+    {
+      "north korea": ["visa", "master"],
+    },
+  ];
+}
